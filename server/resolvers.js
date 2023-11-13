0be8e688ec52a9bfb2f1eaf434d1d0af81d1cbd2
Tabby Toney Douglass <tabbytoney@gmail.com>
2023-11-13 16:30:23 -0600

    add companies to jobs

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,10 +1,12 @@
 import { getJobs } from './db/jobs.js';
+import { getCompany } from './db/companies.js';
 
 export const resolvers = {
   Query: {
     jobs: () => getJobs(),
   },
   Job: {
+    company: (job) => getCompany(job.companyId),
     date: (job) => toIsoDate(job.createdAt),
   },
 };
