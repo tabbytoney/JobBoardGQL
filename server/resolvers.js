e609b57069f5ae77f4544790dde57666851ccb9d
Tabby Toney Douglass <tabbytoney@gmail.com>
2023-11-13 17:02:23 -0600

    backend, add ability to get just one job

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,9 +1,10 @@
-import { getJobs } from './db/jobs.js';
+import { getJobs, getJob } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
 
 export const resolvers = {
   Query: {
     jobs: () => getJobs(),
+    job: (_root, { id }) => getJob(id),
   },
   Job: {
     company: (job) => getCompany(job.companyId),
