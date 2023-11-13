573e84d628559885e6649324240ec117106f2027
Tabby Toney Douglass <tabbytoney@gmail.com>
2023-11-13 16:49:14 -0600

    show query result on frontend

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,21 @@
 import JobList from '../components/JobList';
 import { getJobs } from '../lib/graphql/queries';
+import { useEffect, useState } from 'react';
 
-getJobs().then((jobs) => console.log(jobs));
+// getJobs().then((jobs) => console.log(jobs));
 
 function HomePage() {
+  const [jobs, setJobs] = useState([]);
+
+  // only run (fetch the data) when component is rendered
+  useEffect(() => {
+    getJobs().then(setJobs);
+  }, []);
+
   return (
     <div>
       <h1 className='title'>Job Board</h1>
-      {/* <JobList jobs={jobs} /> */}
+      <JobList jobs={jobs} />
     </div>
   );
 }
