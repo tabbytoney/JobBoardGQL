23af99743dfe10bb6ad7c624a544936ae74d519a
Tabby Toney Douglass <tabbytoney@gmail.com>
2023-11-20 16:38:36 -0600

    using state for loading, success, and error

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -5,15 +5,41 @@ import JobList from '../components/JobList.js';
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const [company, setCompany] = useState();
+  // const [company, setCompany] = useState();
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false,
+  });
 
   useEffect(() => {
-    getCompany(companyId).then(setCompany);
+    // getCompany(companyId).then(setCompany);
+
+    // the () is a away to call the function immediately
+    // and be able to use async within useEffect
+    (async () => {
+      try {
+        const company = await getCompany(companyId);
+        setState({ company, loading: false, error: false });
+      } catch (error) {
+        console.log('error:', JSON.stringify(error, null, 2));
+        setState({ company: null, loading: false, error: true });
+      }
+    })();
   }, [companyId]);
 
   // will error without this bc job is undefined
-  if (!company) {
+  // if (!company) {
+  //   return <div>Loading...</div>;
+  // }
+  console.log('[CompanyPage] state:', state);
+  const { company, loading, error } = state;
+
+  if (loading) {
     return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div className='has-text-danger'>Data unavailable</div>;
   }
 
   // for use with fake data
